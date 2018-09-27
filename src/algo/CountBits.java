31e1f69d0e0bab8b6c4e97583a528ef697641bf2
Rohit Raj <rohitrjsinha@gmail.com>
2018-09-27 19:32:13 +0530

    Master: count all sets bits from 1 to n

---
+++ src/algo/CountBits.java
@@ -0,0 +1,31 @@
+package algo;
+
+/**
+ * https://www.geeksforgeeks.org/count-total-set-bits-in-all-numbers-from-1-to-n/
+ */
+class CountBits {
+
+    static int countSetBits(int a){
+        int count = 0;
+
+        while (a > 0){
+            a &= (a-1);
+            count++;
+        }
+        return count;
+    }
+
+    static int countSetBitsUtil(int x) {
+        int count = 0;
+        for(int i = 1; i <= x; i++){
+            count += countSetBits(i);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        int a = 4;
+        System.out.println("set bit count = " + countSetBitsUtil(a));
+
+    }
+}
