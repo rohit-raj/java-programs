f3ca1baa45e02a3e59d402a4ef40c652402ee757
Rohit Raj <rohitrjsinha@gmail.com>
2018-09-27 15:15:59 +0530

    Master: multiplication using bit manipulation

---
+++ src/algo/BitMultiplication.java
@@ -0,0 +1,25 @@
+package algo;
+
+class BitMultiplication {
+    int mul(int a, int b) {
+        if (b == 1) {
+            return a;
+        }
+
+        int nextPowerOfTwoForYourNumber = 1;
+        int numberOfBits = 0;
+
+        while (nextPowerOfTwoForYourNumber < b) {
+            nextPowerOfTwoForYourNumber = nextPowerOfTwoForYourNumber << 1;
+            numberOfBits++;
+        }
+        return (a << numberOfBits) - (mul(a, (nextPowerOfTwoForYourNumber - b)));
+    }
+
+    public static void main(String[] args) {
+        BitMultiplication bit = new BitMultiplication();
+        int a = 2;
+        int b = 5;
+        System.out.printf("multiple of " + a + " * " + b + " = " + bit.mul(a, b));
+    }
+}
